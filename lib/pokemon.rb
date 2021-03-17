677ba489aeb6e3966feba9ed0ac9733a637d87e6
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-17 22:59:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db 
+  attr_accessor :id, :name, :type, :db, :hp 
   
-  def initialize(db)
+  def initialize(id, name, type, db, hp)
     @id = id 
     @name = name 
     @type = type 
